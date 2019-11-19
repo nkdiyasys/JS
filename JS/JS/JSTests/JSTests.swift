8549748573a31d5ee6b18879d677de0a4a68957d
nkdiyasys <nkdiyasys@gmail.com>
2019-11-19 16:00:13 +0530

    added test case

---
+++ JS/JS/JSTests/JSTests.swift
@@ -0,0 +1,39 @@
+//
+//  JSTests.swift
+//  JSTests
+//
+//  Created by Nithin on 19/11/19.
+//  Copyright © 2019 Nithin. All rights reserved.
+//
+
+import XCTest
+
+class JSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testAdd(){
+        let a = 10
+        let b = 30
+        let sum = a + b
+        XCTAssertTrue(sum == 40, "sum")
+    }
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
